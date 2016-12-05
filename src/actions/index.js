6589f6a5841a13481c6734e4d4d6ddce250e0713
Apollo Tang <tang.apollo@gmail.com>
2016-12-05 14:20:52 -0500

    add whatwg-fetch

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,14 @@
 import { v4 } from 'node-uuid';
+import 'whatwg-fetch';
+
+(function(){
+fetch('https://api.github.com/repos/vmg/redcarpet/issues?state=closed')
+    .then((response) => response.json())
+    .then((responseData) => {
+        console.log('xxxx', responseData );
+      // this.setState({contacts: responseData});
+    })
+})();
 
 export const addTodo = (text) => ({
   type: 'ADD_TODO',
